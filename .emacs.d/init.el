819aa3758136b60d5c5b0c622d014805bf29b3fa
Nicolas Ginder <nicolas-ginder@users.noreply.github.com>
2016-12-02 22:30:47 +0000

    cleanup emacs config and fix i3 path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,7 @@
     (package-install pkg)))
 
 
-;(package-require 'helm)
+(package-require 'helm)
 (package-require 'helm-swoop)
 (package-require 'cyberpunk-theme)
 (package-require 'dired+)
@@ -23,6 +23,7 @@
 (package-require 'restclient)
 (package-require 'rainbow-delimiters)
 (package-require 'jq-mode)
+(package-require 'haskell-mode)
 
 (tool-bar-mode -1)
 (load-theme 'cyberpunk 1)
@@ -54,7 +55,8 @@
 
 (require 'recentf)
 (recentf-mode 1)
-(run-at-time nil (* 30) 'recentf-save-list)
+(run-at-time nil (* 1 60) 'recentf-save-list)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -63,7 +65,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (haskell-mode restclient rainbow-delimiters pdf-tools js2-mode jq-mode helm-swoop dired+ cyberpunk-theme))))
+    (haskell-mode restclient rainbow-delimiters js2-mode jq-mode helm-swoop dired+ cyberpunk-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
