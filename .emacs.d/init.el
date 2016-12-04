db06df26cae2776c045aafb99bbff6c14d48b793
Nicolas Ginder <nicolas-ginder@users.noreply.github.com>
2016-12-04 22:48:52 +0000

    resolve mac conflict and setup haskell

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,14 +58,44 @@
 (run-at-time nil (* 1 60) 'recentf-save-list)
 
 
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(require 'flymake-haskell-multi)
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+
+(defun turn-on-subword-mode ()
+  (interactive)
+  (subword-mode 1))
+(defun my-haskell-mode-hook ()
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+;  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+  (flymake-haskell-multi-load)
+  (ghc-init))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-mode-hook
+   (quote
+					;    (interactive-haskell-mode turn-on-haskell-indent turn-on-subword-mode turn-on-haskell-decl-scan my-haskell-mode-hook)))
+        (interactive-haskell-mode turn-on-subword-mode turn-on-haskell-decl-scan my-haskell-mode-hook)))
+ '(haskell-process-type (quote cabal-repl))
  '(package-selected-packages
    (quote
-    (haskell-mode restclient rainbow-delimiters js2-mode jq-mode helm-swoop dired+ cyberpunk-theme))))
+    (flymake-haskell-multi haskell-mode restclient rainbow-delimiters js2-mode jq-mode helm-swoop dired+ cyberpunk-theme))))
+
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
